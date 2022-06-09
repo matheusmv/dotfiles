7dfea54de38d4e2408aa2d2711de2d857b1f820f
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-06-09 19:08:09 -0300

    update bin scripts

--- .bin/arch-updates.sh
+++ .bin/arch-updates.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-updates=$(checkupdates | wc -l ; checkupdates)
+updates=$(checkupdates | wc -l)
 icon=$HOME/.config/dunst/normal.png
 
 notify-send -u normal -i $icon "UPDATES: $updates"
